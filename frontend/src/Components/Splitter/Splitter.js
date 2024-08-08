1c9b7c66125a039df4b0ffba7e4ee62e1625f97d
Sudharshan Bommasani <sudharshan.bommasani@capgemini.com>
2024-08-08 17:17:41 +0530

    splitter

--- frontend/src/Components/Splitter/Splitter.js
+++ frontend/src/Components/Splitter/Splitter.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import "./Splitter.css";
 import Split from "react-split";
 import LeftPaneMenu from "../LeftPaneMenu";
-import Chatbot from "../Chatinterface/ChatBot";
+import ChatBot from "../Chatinterface/ChatBot"
 /*  
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
@@ -24,7 +24,7 @@ function Splitter(){
           config={config}
           messageParser={MessageParser}
           actionProvider={ActionProvider}/>*/}
-           <Chatbot />
+           <ChatBot />
             </div>
         </Split>
     );
